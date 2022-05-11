a7834d98f357b6179bc04d35c6dde39e586df314
B2856 <102830399+B2856@users.noreply.github.com>
2022-05-11 16:22:32 -0400

    Update hurricane_project.py

--- hurricane_project.py
+++ hurricane_project.py
@@ -1,6 +1,4 @@
 # names of hurricanes
-from calendar import c
-
 
 names = ['Cuba I', 'San Felipe II Okeechobee', 'Bahamas', 'Cuba II', 'CubaBrownsville', 'Tampico', 'Labor Day', 'New England', 'Carol', 'Janet', 'Carla', 'Hattie', 'Beulah', 'Camille', 'Edith', 'Anita', 'David', 'Allen', 'Gilbert', 'Hugo', 'Andrew', 'Mitch', 'Isabel', 'Ivan', 'Emily', 'Katrina', 'Rita', 'Wilma', 'Dean', 'Felix', 'Matthew', 'Irma', 'Maria', 'Michael']
 
